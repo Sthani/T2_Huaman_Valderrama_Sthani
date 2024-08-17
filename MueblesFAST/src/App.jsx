dd73d18301a4d3c660d3882ea1afdce24e756441
Sthani <sthanihuamanv13@gmail.com>
2024-08-17 23:49:54 +0000

    Estilo menu

--- MueblesFAST/src/App.jsx
+++ MueblesFAST/src/App.jsx
@@ -12,8 +12,9 @@ function App() {
     <>
 <Menu />
       <div>
+
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={logo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
--- MueblesFAST/src/componentes/Menu.jsx
+++ MueblesFAST/src/componentes/Menu.jsx
@@ -1,22 +1,27 @@
 import { Fragment } from "react";
-import logo from './img/logoM.JPG'
+import './estilos/Menu.css'
 
 
 function Menu(){
     
     return (
         <>
-        
-          <div class="menu">
-        <img src={logo} alt="imagen"/>
 
-        <nav>
-          <a href="#">Quiénes Somos </a>
-          <a href="#"> Tiendas</a>
-          <a href="#"> Servicios</a>
-          <a href="#"> Ofertas</a>
-        </nav>
+
+    <header className="menu">
+      <div className="menu-logo">
+        <img src="/logoM.png" alt="Logo" className="logo-image" /> {/* Asegúrate de tener esta imagen en la carpeta public */}
       </div>
+      <nav className="menu-nav">
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+
 
         </>
     );
